9cb11a382a5baaf004f0199d98f44ad5ef93b8eb
Sohaila Daoud <63005434+sohailadaoud@users.noreply.github.com>
2023-11-04 22:46:15 +0200

    splash screen added.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
-import 'package:chat_app/home_screen.dart';
+import 'package:chat_app/ui/auth/login/login_screen.dart';
+import 'package:chat_app/ui/splash/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,8 +10,20 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: HomeScreen(),
+    return ScreenUtilInit(
+      minTextAdapt: true,
+      splitScreenMode: true,
+      designSize: const Size(400, 700),
+      builder: (context, state) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          initialRoute: SplashScreen.routeName,
+          routes: {
+            SplashScreen.routeName: (context) => SplashScreen(),
+            LoginScreen.routeName: (context) => LoginScreen(),
+          },
+        );
+      },
     );
   }
 }
